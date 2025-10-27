6de9611516ef9b950a708da6710e977b31ef66a7
root <root@ip-172-31-44-207.ec2.internal>
2025-10-27 15:04:27 +0000

    path fixed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,17 @@ module.exports = {
       name: 'expense-backend',
       script: 'server.js',
       cwd: './backend',
-      env: { PORT: 8001 }
+      env: { PORT: 8001 },
+      autorestart: false,
+      stop_exit_codes: [0]
     },
     {
       name: 'expense-frontend',
       script: 'npx',
       args: 'serve -s build -l 3004 -n',
-      cwd: './frontend'
+      cwd: './frontend',
+      autorestart: false,
+      stop_exit_codes: [0]
     }
   ]
-}; 
+};
